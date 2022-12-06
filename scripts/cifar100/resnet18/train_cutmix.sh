1630f355a53c0c9535481c62b90c45944017a9a3
sonbk220 <sonbk220@yonsei.ac.kr>
2022-12-06 04:37:40 +0000

    add cutmix_m

--- scripts/cifar100/resnet18/train_cutmix.sh
+++ scripts/cifar100/resnet18/train_cutmix.sh
@@ -3,10 +3,10 @@
 PORT='tcp://127.0.0.1:12347'
 GPU=0
 SAVEDIR='saved'
-NAME="gpu_1_cutmix"
+NAME="gpu_1_cutmix_m"
 
 DATASET='cifar100'
 
-python train.py -c configs/cifar100/resnet18/config_cutmix.json \
+python train.py -c configs/cifar100/resnet18/config_cutmix_m.json \
 -d ${GPU} --dist_url ${PORT} --save_dir ${SAVEDIR} --name ${NAME} \
 --dataset ${DATASET}
