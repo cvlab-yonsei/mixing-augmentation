58e636f17ab3baf543a02ba6130d0daa91adc28f
dh-baek <sonbk220@yonsei.ac.kr>
2022-12-15 08:17:20 +0000

    add script

---
+++ scripts/cifar100/resnet18/train_puzzlemix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PORT='tcp://127.0.0.1:12347'
+GPU=0
+SAVEDIR='saved'
+NAME="gpu_1_puzzlemix"
+
+DATASET='cifar100'
+
+python train.py -c configs/cifar100/resnet18/config_puzzlemix.json \
+-d ${GPU} --dist_url ${PORT} --save_dir ${SAVEDIR} --name ${NAME} \
+--dataset ${DATASET}
