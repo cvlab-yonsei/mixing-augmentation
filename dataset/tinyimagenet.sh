c3e98e678fcbc606f756f1d692ee2237f0511ad2
dh-baek <sonbk220@yonsei.ac.kr>
2022-12-17 08:57:35 +0000

    add tiny imagenet

---
+++ dataset/tinyimagenet.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+"""
+Code from: https://gist.github.com/moskomule/2e6a9a463f50447beca4e64ab4699ac4
+"""
+
+ROOTDIR='/dataset'
+
+# download and unzip dataset
+cd ${ROOTDIR}
+wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+unzip tiny-imagenet-200.zip
+
+current="$(pwd)/tiny-imagenet-200"
+
+# training data
+cd $current/train
+for DIR in $(ls); do
+   cd $DIR
+   rm *.txt
+   mv images/* .
+   rm -r images
+   cd ..
+done
+
+# validation data
+cd $current/val
+annotate_file="val_annotations.txt"
+length=$(cat $annotate_file | wc -l)
+for i in $(seq 1 $length); do
+    # fetch i th line
+    line=$(sed -n ${i}p $annotate_file)
+    # get file name and directory name
+    file=$(echo $line | cut -f1 -d" " )
+    directory=$(echo $line | cut -f2 -d" ")
+    mkdir -p $directory
+    mv images/$file $directory
+done
+rm -r images
+echo "done"
